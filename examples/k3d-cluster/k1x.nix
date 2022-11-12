d31cc058fb9afaedf7bb959412a7089dbd2a265d
Gustavo Porto <gustavo@portosanti.com>
2022-11-12 12:07:34 +0000

    docs: update examples

--- examples/k3d-cluster/k1x.nix
+++ examples/k3d-cluster/k1x.nix
@@ -5,12 +5,14 @@
   cluster = {
     name = "mycluster";
     provider = "k3d";
+    # https://k3d.io/v5.0.0/usage/configfile/
     nativeConfig = {
-      apiVersion = "k3d.io/v1alpha2";
+      apiVersion = "k3d.io/v1alpha3";
       kind = "Simple";
+      name = "mycluster";
       servers = 1;
       agents = 2;
-      image = "rancher/k3s:v1.22.9-k3s1";
+      image = "rancher/k3s:v1.20.4-k3s1";
       ports = [{
         port = "8080:80";
         nodeFilters = [ "loadbalancer" ];
