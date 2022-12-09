55daa6f77678bfb566ba9a0be21c39d49079b594
Gustavo Porto <gustavo@portosanti.com>
2022-12-09 19:57:33 +0000

    feat: implement parser for top-level options

--- src/modules/default.nix
+++ src/modules/default.nix
@@ -1,5 +1,35 @@
-{ ... }:
+{ config, pkgs, lib, ... }:
 
-{
+let types = lib.types;
+in {
   imports = [ ./cluster.nix ];
+  options = {
+    version = lib.mkOption {
+      type = types.str;
+      default = "0.0.1";
+      description = "Version of the application";
+    };
+    cluster = lib.mkOption {
+      description = "Cluster configuration";
+      type = with types;
+        listOf (submodule {
+          options = {
+            name = lib.mkOption {
+              type = types.str;
+              description = "An arbitrary name for the cluster";
+            };
+            provider = lib.mkOption {
+              type = types.str;
+              description = "Cluster provisioner";
+              default = "k3d";
+            };
+            nativeConfig = lib.mkOption {
+              type = types.attrs;
+              description = "Cluster provisioner configuration";
+              default = { };
+            };
+          };
+        });
+    };
+  };
 }
